b6c3b6155841a587b0d662477fa2d3bc57280322
mhs-first-robotics <mhs.robotics@gmail.com>
2025-01-09 11:41:41 -0800

    Add arm and slide PIDs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleopPOV_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleopPOV_Linear.java
@@ -60,11 +60,10 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
     private DcMotorEx blDrive;
     private DcMotorEx flDrive;
     private DcMotorEx arm;
-    private final PIDController armPidController = new PIDController(1, 0.0, 0.0);
-    private final double ARM_MOTOR_ENCODER_TICKS_PER_REVOLUTION = 1425.1;
-    private final double ARM_OUTPUT_SPROCKET_TEETH = 18;
-    private final double ARM_INPUT_SPROCKET_TEETH = 10;
-
+    private DcMotorEx linear;
+    private final PIDController armPidController = new PIDController(0.002, 0.0, 0.0); // 0.002 power per tick
+    private final PIDController linearPidController = new PIDController(2.5, 0, 0.0); // 2.5 power per one slide extension
+    private final double MAX_LINEAR_SLIDE_ENCODER = 2200;
 
     @Override
     public void runOpMode() {
@@ -75,13 +74,15 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
         blDrive = hardwareMap.get(DcMotorEx.class, "BackLeftDrive");
         flDrive = hardwareMap.get(DcMotorEx.class, "FrontLeftDrive");
         arm = hardwareMap.get(DcMotorEx.class, "arm");
+        linear = hardwareMap.get(DcMotorEx.class, "linear");
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
         blDrive.setDirection(DcMotor.Direction.REVERSE);
         flDrive.setDirection(DcMotor.Direction.REVERSE);
-        arm.setDirection(DcMotor.Direction.REVERSE);
+        arm.setDirection(DcMotor.Direction.FORWARD);
+        //linear.setDirection(DcMotor.Direction.REVERSE); No idea if I will need this, uncomment if neccesary
 
         // If there are encoders connected, switch to RUN_USING_ENCODER mode for greater accuracy
         // leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -108,26 +109,42 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
             blDrive.setPower(fb - lr + turn);
             flDrive.setPower(fb + lr - turn);
 
-            double rotationsAtMotorShaft = arm.getCurrentPosition() / ARM_MOTOR_ENCODER_TICKS_PER_REVOLUTION;
-            double rotationsAtArmShaft = rotationsAtMotorShaft * (ARM_INPUT_SPROCKET_TEETH/ARM_OUTPUT_SPROCKET_TEETH);
+            // ARM FUNCTIONALITY
+            {
+                if (gamepad1.dpad_up) {
+                    armPidController.setPoint = -1000.0;
+                } else if (gamepad1.dpad_down) {
+                    armPidController.setPoint = 0.0;
+                }
 
-            if (gamepad1.dpad_up) {
-                armPidController.setPoint = -0.25;
-            } else if (gamepad1.dpad_down) {
-                armPidController.setPoint = 0.0;
+                armPidController.update(arm.getCurrentPosition()); // tell the PID controller the arm's current position
+                double staticFrictionTerm = -Math.signum(armPidController.getOutput()) * 0.05;
+                // set point in ticks, times 1 half-revolution per 1000 ticks, times pi radians per half-revolution
+                double counteractGravityTerm = Math.cos((arm.getCurrentPosition() / 2000.0) * Math.PI) * 0.3;
+                // negative because for some reason, reversing the motor direction also reverses the encoder
+                arm.setPower(-armPidController.getOutput() + staticFrictionTerm + counteractGravityTerm);
+
+                telemetry.addData("Arm PID Output", armPidController.getOutput());
+                telemetry.addData("Arm PID Setpoint", armPidController.setPoint);
+                telemetry.addData("Arm Position", arm.getCurrentPosition());
             }
 
-            armPidController.update(rotationsAtArmShaft);
-            //arm.setPower( armPidController.getOutput() );
+            // LINEAR SLIDE FUNCTIONALITY
+            {
+                double percent = linear.getCurrentPosition() / MAX_LINEAR_SLIDE_ENCODER;
+                telemetry.addData("linear slide position", percent);
 
-            telemetry.addData("Arm PID Output", armPidController.getOutput());
-            telemetry.addData("Arm PID Setpoint", armPidController.setPoint);
-            telemetry.addData("Arm Position", rotationsAtArmShaft);
+                if (gamepad1.right_trigger > 0.2) {
+                    linearPidController.setPoint = 1.0;
+                } else if (gamepad1.left_trigger > 0.2){
+                    linearPidController.setPoint = 0.0;
+                }
+
+                linearPidController.update(percent);
+                linear.setPower(linearPidController.getOutput());
+            }
 
             telemetry.update();
-
-            // Pace this loop so jaw action is reasonable speed.
-            sleep(50);
         }
     }
 }
