cf8edf98599fd80e3af18c57c5366dbbfaf1ea43
ewyzzia <giikae127@gmail.com>
2025-02-03 14:48:38 -0800

    bang

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleopPOV_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleopPOV_Linear.java
@@ -29,14 +29,13 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 /*
  * This OpMode executes a POV Game style Teleop for a direct drive robot
@@ -50,6 +49,13 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
+enum DriveMode {
+    DRIVE,
+    DEPOSIT_BASKET,
+    INTAKE,
+    MANUAL_OVERRIDE
+}
+
 @TeleOp(name="Robot: Teleop POV", group="Robot")
 //@Disabled
 public class RobotTeleopPOV_Linear extends LinearOpMode {
@@ -61,9 +67,22 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
     private DcMotorEx flDrive;
     private DcMotorEx arm;
     private DcMotorEx linear;
+    private Servo wristRoll;
+    private Servo wristPitch;
+    private CRServo intake;
+
     private final PIDController armPidController = new PIDController(0.002, 0.0, 0.0); // 0.002 power per tick
-    private final PIDController linearPidController = new PIDController(2.5, 0, 0.0); // 2.5 power per one slide extension
+    private final PIDController linearPidController = new PIDController(25, 0, 0.0); // 2.5 power per one slide extension
     private final double MAX_LINEAR_SLIDE_ENCODER = 2200;
+
+    private DriveMode currentDriveMode = DriveMode.DRIVE;
+    private double driveSpeedMult = 1.0;
+
+    private Gamepad currentGamepad = new Gamepad();
+    private Gamepad lastGamepad = new Gamepad();
+
+    private double lastUpdateTime = 0.0;
+
 
     @Override
     public void runOpMode() {
@@ -75,6 +94,9 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
         flDrive = hardwareMap.get(DcMotorEx.class, "FrontLeftDrive");
         arm = hardwareMap.get(DcMotorEx.class, "arm");
         linear = hardwareMap.get(DcMotorEx.class, "linear");
+        wristRoll = hardwareMap.get(Servo.class, "wristRoll");
+        wristPitch = hardwareMap.get(Servo.class, "wristPitch");
+        intake = hardwareMap.get(CRServo.class, "intake");
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
@@ -82,6 +104,7 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
         blDrive.setDirection(DcMotor.Direction.REVERSE);
         flDrive.setDirection(DcMotor.Direction.REVERSE);
         arm.setDirection(DcMotor.Direction.FORWARD);
+        intake.setDirection(DcMotorSimple.Direction.REVERSE);
         //linear.setDirection(DcMotor.Direction.REVERSE); No idea if I will need this, uncomment if neccesary
 
         // If there are encoders connected, switch to RUN_USING_ENCODER mode for greater accuracy
@@ -98,24 +121,124 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            // Run wheels in POV mode (note: The joystick goes negative when pushed forward, so negate it)
-            // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
-            // This way it's also easy to just drive straight, or just turn.
-            double fb = -gamepad1.left_stick_y;
-            double lr = gamepad1.left_stick_x;
-            double turn = gamepad1.right_stick_x;
-            frDrive.setPower(fb - lr - turn);
-            brDrive.setPower(fb + lr - turn);
-            blDrive.setPower(fb - lr + turn);
-            flDrive.setPower(fb + lr - turn);
+            double runtime = getRuntime();
+            double deltaTime = runtime - lastUpdateTime;
+            lastUpdateTime = runtime;
 
-            // ARM FUNCTIONALITY
-            {
-                if (gamepad1.dpad_up) {
-                    armPidController.setPoint = -1000.0;
-                } else if (gamepad1.dpad_down) {
-                    armPidController.setPoint = 0.0;
+            lastGamepad.copy(currentGamepad);
+            currentGamepad.copy(gamepad1);
+
+            double slidePosition = linear.getCurrentPosition() / MAX_LINEAR_SLIDE_ENCODER;
+
+            switch (currentDriveMode) {
+                case DRIVE: {
+
+                    driveSpeedMult = 1.0;
+                    linearPidController.setPoint = 0.0;
+                    // only pivot down if slides are retracted; don't want to tip over
+                    if (slidePosition < 0.3) {
+                        armPidController.setPoint = 0.0;
+                    }
+
+                    wristRoll.setPosition(0.6);
+                    wristPitch.setPosition(0.5);
+                    intake.setPower( 0.0 );
+
+                    if (currentGamepad.start && !lastGamepad.start) {
+                        currentDriveMode = DriveMode.MANUAL_OVERRIDE;
+                    } else if (currentGamepad.dpad_up && !lastGamepad.dpad_up) {
+                        currentDriveMode = DriveMode.DEPOSIT_BASKET;
+                    } else if (currentGamepad.dpad_down && !lastGamepad.dpad_down) {
+                        currentDriveMode = DriveMode.INTAKE;
+                    }
+
+                    break;
                 }
+                case DEPOSIT_BASKET: {
+
+                    driveSpeedMult = 0.5;
+                    armPidController.setPoint = -1000.0;
+                    boolean isArmInThreshold = Math.abs(arm.getCurrentPosition() - armPidController.setPoint) < 100.0;
+
+                    // only extend if arm is pivoted up; once again, don't want to tip over
+                    if (isArmInThreshold) {
+                        linearPidController.setPoint = 1.0;
+                    }
+
+                    wristRoll.setPosition(0.5);
+                    wristPitch.setPosition(0.8);
+
+                    intake.setPower( -gamepad1.left_trigger );
+
+                    if (currentGamepad.dpad_down && !lastGamepad.dpad_down) {
+                        currentDriveMode = DriveMode.DRIVE;
+                    }
+
+                    break;
+                }
+                case INTAKE: {
+
+                    driveSpeedMult = 0.5;
+                    linearPidController.setPoint = 0.45;
+                    armPidController.setPoint = 170;
+
+                    // don't turn the wrist until extended a bit; it might catch on the chassis otherwise
+                    if (slidePosition > 0.1) {
+                        wristRoll.setPosition(0.5);
+                        wristPitch.setPosition(0.6);
+                    }
+
+                    intake.setPower( gamepad1.right_trigger - gamepad1.left_trigger );
+
+                    if (currentGamepad.dpad_up && !lastGamepad.dpad_up) {
+                        currentDriveMode = DriveMode.DRIVE;
+                    }
+
+                    break;
+                }
+                case MANUAL_OVERRIDE: {
+
+                    arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                    linear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+                    double armMoveDir = (currentGamepad.right_bumper ? 1.0 : 0.0) - (currentGamepad.left_bumper ? 1.0 : 0.0);
+                    arm.setPower( armMoveDir * 0.35 );
+                    linear.setPower( (currentGamepad.right_trigger - currentGamepad.left_trigger) * 0.3 );
+
+                    double wristRollMoveDir = (currentGamepad.dpad_right ? 1.0 : 0.0) - (currentGamepad.dpad_left ? 1.0 : 0.0);
+                    wristRoll.setPosition(wristRoll.getPosition() + wristRollMoveDir * deltaTime * 4.0 );
+                    double wristPitchMoveDir = (currentGamepad.dpad_up ? 1.0 : 0.0) - (currentGamepad.dpad_down ? 1.0 : 0.0);
+                    wristPitch.setPosition(wristPitch.getPosition() + wristPitchMoveDir * deltaTime * 4.0 );
+
+                    if (currentGamepad.x) {
+                        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                        arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                        linear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                        linear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    }
+                    if (currentGamepad.start && !lastGamepad.start) {
+                        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+                        linear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+                        currentDriveMode = DriveMode.DRIVE;
+                    }
+
+                    break;
+                }
+            }
+
+            // DRIVETRAIN FUNCTIONALITY
+            {
+                double fb = -currentGamepad.left_stick_y * driveSpeedMult;
+                double lr = currentGamepad.left_stick_x * driveSpeedMult;
+                double turn = -currentGamepad.right_stick_x * driveSpeedMult;
+                frDrive.setPower(fb - lr - turn);
+                brDrive.setPower(fb + lr - turn);
+                blDrive.setPower(fb - lr + turn);
+                flDrive.setPower(fb + lr + turn);
+            }
+
+            // ARM PID FUNCTIONALITY
+            if (currentDriveMode != DriveMode.MANUAL_OVERRIDE) {
 
                 armPidController.update(arm.getCurrentPosition()); // tell the PID controller the arm's current position
                 double staticFrictionTerm = -Math.signum(armPidController.getOutput()) * 0.05;
@@ -129,18 +252,11 @@ public class RobotTeleopPOV_Linear extends LinearOpMode {
                 telemetry.addData("Arm Position", arm.getCurrentPosition());
             }
 
-            // LINEAR SLIDE FUNCTIONALITY
-            {
-                double percent = linear.getCurrentPosition() / MAX_LINEAR_SLIDE_ENCODER;
-                telemetry.addData("linear slide position", percent);
+            // LINEAR SLIDE PID FUNCTIONALITY
+            if (currentDriveMode != DriveMode.MANUAL_OVERRIDE) {
+                telemetry.addData("linear slide position", slidePosition);
 
-                if (gamepad1.right_trigger > 0.2) {
-                    linearPidController.setPoint = 1.0;
-                } else if (gamepad1.left_trigger > 0.2){
-                    linearPidController.setPoint = 0.0;
-                }
-
-                linearPidController.update(percent);
+                linearPidController.update(slidePosition);
                 linear.setPower(linearPidController.getOutput());
             }
 
